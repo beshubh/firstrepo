416548bb5538f47db04fb3b52e99e88c07cfa690
shubhamsks <39399239+shubhamsks@users.noreply.github.com>
2018-11-21 18:49:39 +0530

    seemsInterestingHere

--- first_java(SK)@bla.java
+++ first_java(SK)@bla.java
@@ -4,5 +4,10 @@ public static void main(String args[]){
 System.out.print("Hello Git");
 System.out.println("first change");
   System.out.println("ok this is the change may be :)");
+  for(int i=0;i<20;i++){
+  System.out.println("ok it seems prety intesting");
+    
+  }
 }
+  
 }
